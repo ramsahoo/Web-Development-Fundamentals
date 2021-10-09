a2c7a436eec50310487c254f2c68958ba3b22eae
Rameen Sahoo <rameensahoo@gmail.com>
2021-10-08 20:16:21 -0700

    added all files from web development fundamentals course

---
+++ js/javascript-hello-world.js
@@ -0,0 +1,8 @@
+function getName() {
+    const userName = prompt("what is your name?");
+    sayHello(userName);
+}
+function sayHello(name) {
+    alert("Hello " + name + "!");
+    console.log("Alert Sent!");
+}
